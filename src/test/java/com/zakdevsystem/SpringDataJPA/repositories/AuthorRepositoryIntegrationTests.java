c7729d2fb49bbdf0d75be6c83bf3dc051de6fee1
MyDevSystem <rawaha.coder@gmail.com>
2025-08-11 09:51:34 +0100

    remove @GeneratedValue from entity to pass test

---
+++ src/test/java/com/zakdevsystem/SpringDataJPA/repositories/AuthorRepositoryIntegrationTests.java
@@ -0,0 +1,36 @@
+package com.zakdevsystem.SpringDataJPA.repositories;
+
+import com.zakdevsystem.SpringDataJPA.TestDataUtil;
+import com.zakdevsystem.SpringDataJPA.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AuthorRepositoryIntegrationTests {
+
+    private AuthorRepository underTest;
+
+    @Autowired
+    public AuthorRepositoryIntegrationTests(AuthorRepository underTest) {
+        this.underTest = underTest;
+    }
+
+    @Test
+    public void testThatAuthorCanBeCreatedAndRecalled() {
+        Author author = TestDataUtil.createTestAuthorA();
+        underTest.save(author);
+        Optional<Author> result = underTest.findById(author.getId());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(author);
+    }
+}
